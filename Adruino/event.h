58a53ca33316a32c735f516c54cad5471c971b13
Greg Cunningham <greg@curioustech.net>
2016-05-05 03:13:03 -0400

    Update event.h

--- Adruino/event.h
+++ Adruino/event.h
@@ -25,36 +25,52 @@ SOFTWARE.
 
 #define CLIENTS 4
 
+enum Cl_type
+{
+  Type_Normal,
+  Type_PushInstant,
+  Type_Critical
+};
+
 class eventClient
 {
 public:
   eventClient(){}
-  void set(WiFiClient cl, int t);
-  bool inUse();
-  void push(String s);
-  void print(String s);
-  void beat(String s);
-  void alert(String s);
+  void set(WiFiClient cl, int t, uint8_t nType);
+  bool inUse(void);
+  void push(void);  // event: state, data: JSON.   Push the jscript data
+  void pushInstant(void); // push changes instant if push=1
+  void print(String s);   // event: print, data: text  Print anything
+  void beat(void);        // 10 second keepalive
+  void alert(String s);   // event: alert, data: text
+  String (*jsonCallback)(void); // all the data for pushes
 private:
   WiFiClient m_client;
-  int8_t m_keepAlive;
-  uint16_t m_interval;
-  uint16_t m_timer;
+  uint8_t  m_keepAlive; // keep-alive timer (10 seconds)
+  uint8_t  m_nType;     // type 1 = critical connection
+  uint16_t m_interval;  // interval to send full json created from jsonCallback
+  uint16_t m_timer;     // timer for interval
 };
 
 class eventHandler
 {
 public:
-  eventHandler(String (*callback)(void))
+  eventHandler(String (*callback)(void) ) : m_critical_timer(0), m_timeout(0)
   {
-    jsonCallback = callback;
+    for(int i = 0; i < CLIENTS; i++)
+    {
+      ec[i].jsonCallback = callback;
+    }
   }
-  void set(WiFiClient c, int interval);
-  void heartbeat();
-  void push(); // push to all
+  void set(WiFiClient c, int interval, uint8_t nType);
+  void heartbeat(void);
+  void push(void); // push to all
+  void pushInstant(void); // push changes to all if push=1
   void print(String s); // print remote debug
+  void printf(const char *format, ...); // formatted print
   void alert(String s); // send alert
 private:
-  String (*jsonCallback)();
   eventClient ec[CLIENTS];
+  uint16_t m_critical_timer;
+  uint16_t m_timeout;
 };
