60b682e6a3941495b17f4bd60c14211e42721222
Greg Cunningham <greg@curioustech.net>
2016-09-24 07:16:18 -0400

    Create eeMem.h

---
+++ Adruino/eeMem.h
@@ -0,0 +1,47 @@
+#ifndef EEMEM_H
+#define EEMEM_H
+
+#include <Arduino.h>
+
+struct Sched
+{
+  uint16_t setTemp;
+  uint16_t timeSch;
+  uint8_t thresh;
+  uint8_t wday;  // Todo: weekday 0=any, 1-7 = day of week
+};
+
+#define MAX_SCHED 8
+
+struct eeSet // EEPROM backed data
+{
+  uint16_t size;          // if size changes, use defauls
+  uint16_t sum;           // if sum is diiferent from memory struct, write
+  char     szSSID[32];
+  char     szSSIDPassword[64];
+  uint16_t vacaTemp;       // vacation temp
+  int8_t  tz;            // Timezone offset from your global server
+  uint8_t schedCnt;    // number of active scedules
+  bool    bVaca;         // vacation enabled
+  bool    bAvg;         // average target between schedules
+  bool    bEnableOLED;
+  bool    bRes;
+  char    schNames[MAX_SCHED][16]; // 128  names for small display
+  Sched   schedule[MAX_SCHED];  // 48 bytes
+  uint32_t ppkwh, res;
+};
+
+extern eeSet ee;
+
+class eeMem
+{
+public:
+  eeMem();
+  void update(void);
+private:
+  uint16_t Fletcher16( uint8_t* data, int count);
+};
+
+extern eeMem eemem;
+
+#endif // EEMEM_H
