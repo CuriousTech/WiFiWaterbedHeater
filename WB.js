dddb68acbdbfeab9b54c2ccbc52a97fc922cd5ff
Greg Cunningham <greg@curioustech.net>
2018-10-15 05:58:12 -0400

    websocket

--- WB.js
+++ WB.js
@@ -1,7 +1,6 @@
-// Waterbed stream listener using PngPagic (http://www.curioustech.net/pngmagic.html)
-wbIP = '192.168.0.104'
-Url = 'http://' + wbIP + ':82/events'
-var event
+// Waterbed stream listener using PngMagic (http://www.curioustech.net/pngmagic.html)
+wbIP = '192.168.0.116:82'
+Url = 'ws://' + wbIP + '/ws'
 var last
 
 heartbeat = (new Date()).valueOf()
@@ -18,70 +17,48 @@ function OnCall(msg, event, data)
 {
 	switch(msg)
 	{
+		case 'HTTPCONNECTED':
+			Pm.Echo('WB connected')
+			break
 		case 'HTTPDATA':
 			heartbeat = new Date()
-			if(data.length <= 2) break // keep-alive heartbeat
-			lines = data.split('\n')
-			for(i = 0; i < lines.length; i++)
-				procLine(lines[i])
+			procLine(data)
 			break
 		case 'HTTPCLOSE':
-			Http.Connect( 'event', Url )  // Start the event stream
+			Pm.Echo('WB closed')
 			break
 		case 'HTTPSTATUS':
-			Pm.Echo('WB HTTP status ' + event)
+			Pm.Echo('WB HTTP status ' + data)
 			break
 	}
 }
 
 function procLine(data)
 {
-	data = data.replace(/\n|\r/g, "")
 	if(data.length < 2) return
-	if(data == ':ok' )
-	{
-		Pm.Echo( 'WB stream started')
-		return
-	}
+	data = data.replace(/\n|\r/g, "")
+	parts = data.split(';')
 
-	if( data.indexOf( 'event' ) >= 0 )
-	{
-		event = data.substring( data.indexOf(':') + 2)
-		return
-	}
-	else if( data.indexOf( 'data' ) >= 0 )
-	{
-		data = data.substring( data.indexOf(':') + 2)
-	}
-	else
-	{
-		return // headers
-	}
-
-	switch(event)
+	switch(parts[0])
 	{
 		case 'state':
-			LogWB(data)
+			LogWB(parts[1])
 			if(Pm.FindWindow('Waterbed'))
 				Pm.Waterbed('REFRESH')
 			break
 		case 'alert':
-			Pm.Echo('WB Alert: ' + data)
+			Pm.Echo('WB Alert: ' + parts[1])
 			Pm.Beep(0)
 			break
 		case 'print':
-			Pm.Echo('WB ' + data)
-			break
-		case 'request':
-			dumpReq(data)
+			Pm.Echo('WB ' + parts[1])
 			break
 		case 'hack':
 			hackJson = !(/[^,:{}\[\]0-9.\-+Eaeflnr-u \n\r\t]/.test(
-				data.replace(/"(\\.|[^"\\])*"/g, ''))) && eval('(' + data + ')')
+				parts[1].replace(/"(\\.|[^"\\])*"/g, ''))) && eval('(' + parts[1] + ')')
 			Pm.Echo('WB Hack: ' + hackJson.ip + ' ' + hackJson.pass)
 			break
 	}
-	event = ''
 }
 
 function OnTimer()
@@ -89,18 +66,17 @@ function OnTimer()
 	time = (new Date()).valueOf()
 	if(time - heartbeat > 120*1000)
 	{
-		Pm.Echo('WB timeout' )
 		if(!Http.Connected)
 			Http.Connect( 'event', Url )  // Start the event stream
 	}
 }
 
-function 	LogWB(str)
+function LogWB(str)
 {
 	wbJson = !(/[^,:{}\[\]0-9.\-+Eaeflnr-u \n\r\t]/.test(
 		str.replace(/"(\\.|[^"\\])*"/g, ''))) && eval('(' + str + ')')
 
-	line = wbJson.waterTemp + ',' + wbJson.hiTemp + ',' + wbJson.loTemp + ',' + wbJson.on+ ',' + wbJson.temp
+	line = wbJson.waterTemp + ',' + wbJson.hiTemp + ',' + wbJson.loTemp + ',' + wbJson.on+ ',' + wbJson.temp+','+wbJson.rh
 
 	if(+wbJson.temp == 6553.5 || +wbJson.temp == 0)
 		return
@@ -108,44 +84,5 @@ function 	LogWB(str)
 	if(line == last)
 		return
 	last = line
-
-	fso = new ActiveXObject( 'Scripting.FileSystemObject' )
-	tf = fso.OpenTextFile( 'Waterbed.log', 8, true)
-	tf.WriteLine( wbJson.t + ',' + line + ',' + wbJson.rh)
-	tf.Close()
-	fso = null
-}
-
-function dumpReq(data)
-{
-	Json = !(/[^,:{}\[\]0-9.\-+Eaeflnr-u \n\r\t]/.test(
-		data.replace(/"(\\.|[^"\\])*"/g, ''))) && eval('(' + data + ')')
-
-	Pm.Echo('WB Req: R=' + Json.remote + ' H=' + Json.host)
-	Pm.Echo('  URI=' + Json.url)
-//	if(Json.header)
-//	 for(i = 0; i < Json.header.length; i++)
-//		Pm.Echo('  Header ' + i + ': ' +Json.header[i])
-	if(Json.params)
-	 for(i = 0; i < Json.params.length; i++)
-		Pm.Echo('  Param ' + i + ': ' +Json.params[i])
-
-	ip = Json.host
-	if(ip.search('.com')) // reduce to just IP
-	{
-		ip = ip.replace( /[a-z.]/g, '' )
-		ip = ip.replace( /[-]/g, '.' )
-		ip = ip.slice(1)
-	}
-
-	if( ValidateIPaddress(ip) && Reg.localIP != ip && ip != wbIP)
-	{
-		Reg.localIP = ip
-		Pm.Echo('New IP: ' + Reg.localIP)
-	}
-}
-
-function ValidateIPaddress(ip) 
-{  
-	return  (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ip))
+	Pm.Log( 'Waterbed.log', wbJson.t + ',' + line + ',' + wbJson.tc+','+wbJson.l)
 }
