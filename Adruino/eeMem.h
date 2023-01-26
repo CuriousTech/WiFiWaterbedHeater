0c3b1d57917e9e1e9866449125bc31e90e2c4ad1
Greg Cunningham <greg@curioustech.net>
2023-01-26 06:27:18 -0500

    Update eeMem.h

--- Adruino/eeMem.h
+++ Adruino/eeMem.h
@@ -23,32 +23,7 @@ struct Alarm
 
 #define MAX_SCHED 8
 
-struct eeSet // EEPROM backed data
-{
-  uint16_t size;          // if size changes, use defauls
-  uint16_t sum;           // if sum is diiferent from memory struct, write
-  char     szSSID[32];
-  char     szSSIDPassword[64];
-  uint16_t vacaTemp;       // vacation temp
-  int8_t  tz;            // Timezone offset from your global server
-  uint8_t schedCnt;    // number of active scedules
-  bool    bVaca;         // vacation enabled
-  bool    bAvg;         // average target between schedules
-  bool    bEnableOLED;
-  bool    bEco;         // eco mode
-  Sched   schedule[MAX_SCHED];  // 22x8 bytes
-  uint16_t ppkwh;
-  uint16_t rate;
-  uint16_t watts;
-  uint16_t ppkwm[12];
-  uint32_t tSecsMon[12];    // total secwatt hours per month
-  int16_t tAdj[2];
-  int16_t pids[3];
-  int8_t res[8];
-  Alarm   alarm[MAX_SCHED];
-// end of CRC
-  uint16_t lightLevel[2];
-};
+#define EESIZE (offsetof(eeMem, end) - offsetof(eeMem, size) )
 
 class eeMem
 {
@@ -58,8 +33,45 @@ public:
   bool verify(bool bComp);
 private:
   uint16_t Fletcher16( uint8_t* data, int count);
+public:
+  uint16_t size = EESIZE;          // if size changes, use defauls
+  uint16_t sum = 0xAAAA;           // if sum is diiferent from memory struct, write
+  char     szSSID[32];
+  char     szSSIDPassword[64];
+  uint16_t vacaTemp = 700;       // vacation temp
+  int8_t  tz = -5;            // Timezone offset from your global server
+  uint8_t schedCnt = 5;    // number of active scedules
+  bool    bVaca = false;         // vacation enabled
+  bool    bAvg = true;         // average target between schedules
+  bool    bEnableOLED = true;
+  bool    bEco = true;         // eco mode
+  Sched   schedule[MAX_SCHED] =  // 22x8 bytes
+  {
+    {825,  3*60, 3, 0, "Midnight"},
+    {815,  6*60, 2, 0, "Early"},  // temp, time, thresh, wday
+    {810,  8*60, 3, 0, "Morning"},
+    {810, 16*60, 3, 0, "Day"},
+    {825, 21*60, 3, 0, "Night"},
+    {830,  0*60, 3, 0, "Sch6"},
+    {830,  0*60, 3, 0, "Sch7"},
+    {830,  0*60, 3, 0, "Sch8"}
+  };
+  uint16_t ppkwh = 152;
+  uint16_t rate = 58;
+  uint16_t watts =  290; // watts (mbr = 290  GBR = 352);
+  uint16_t ppkwm[12] = {0};
+  uint32_t tSecsMon[12] = {0};    // total secwatt hours per month
+  int16_t tAdj[2] = {0};
+  int16_t pids[3] = {60*3,60*1, 5}; //  {60*5,60*10, 20} // GBR pids
+  int8_t res[8] = {0};
+  Alarm   alarm[MAX_SCHED] =
+  {
+    {0, 1000, 8*60, 0x3E,"Weekday"},
+    {0},
+  };
+
+  uint8_t end;
 };
 
-extern eeMem eemem;
-extern eeSet ee;
+extern eeMem ee;
 #endif // EEMEM_H
